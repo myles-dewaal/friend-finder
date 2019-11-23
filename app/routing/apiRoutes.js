44eb41fefb2c212bcf08f618306ba76c119f2741
Myles Dewaal <52334937+myles-dewaal@users.noreply.github.com>
2019-11-22 18:25:24 -0700

    added home page, and fixed routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,13 @@
-var friendsData = require("../data/friends.js");
-
+var friendData = require('../data/friends.js');
 
 module.exports = function(app) {
 
+    app.get('/api/friends',function(req, res) {
+        res.json(friendData);
+    });
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
-
-
-  app.post("/api/tables", function(req, res) {
-
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+    app.post('/api/friends', function(req,res) {
+        friendData.push(req.body);
+    });
 
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,16 @@
-var path = require("path");
+var path = require('path');
+
+module.exports = function(app) { 
+   
+//Set up a default, catch-all route that leads to home.html
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/home.html"));
+});
+
+//Set up a get route to /survey to display the survey page. 
+app.get("/survey", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/survey.html"));
+});
 
 
-
-module.exports = function(app) {
-
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
-  });
-
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-};
+}
